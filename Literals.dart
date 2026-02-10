2b5200a4595b99aef1ffd8d85b477f78c27e6256
rishirich4090 <mislol4090@gmail.com>
2026-02-10 18:58:02 +0530

    day 3

---
+++ Literals.dart
@@ -0,0 +1,13 @@
+//Create a string using literals and print it
+//Use string interpolation to print:
+void main() {
+  String z = "Rishi";
+  String t = "My name:";
+  print("$z $t I am student");
+  int c = 10;
+  double r = 23.87;
+  print("Sum of $c + $r is ${c + r}");
+  print("Length of String t= ${t.length}");
+}
+
+//Create two numbers and display their sum using interpolation.
--- fourth.dart
+++ fourth.dart
@@ -1,4 +1,54 @@
 //Write a program that prints the following pattern:
+// void main() {
+//   print("HELLO\nWORLD\nrishi\ndart hello\npattern");
+// }
+//Create a program that prints a welcome message dynamically like:
+// void main() {
+//   var x = "Rishi";
+//   print("My Name is $x Welcome to dart programing language");
+// }
+// void main() {
+//   String x = "rishi";
+//   int y = 10;
+//   double z = 10.123456;
+//   bool is_student = true;
+//   print("$x:$y:$z:$is_student");
+// }
+// void main() {
+//   var x = 7;
+//   var r = 6;
+//   print("X+R=${x + r}");
+//   print("x*r=${x * r}");
+//   print("x-r=${x - r}");
+//   print("x/r=${x / r}");
+// }
+//Store a price in a double variable and convert it into an int.
+
 void main() {
-  print("HELLO\nWORLD\nrishi\ndart hello\npattern");
+  //   String x = "Rishi";
+  //   print(x);
+  //   int y = x.toInt();
+  //   print(y);
+
+  // Example of converting String to int
+  //   String x = "123";
+  //   print(x);
+  //   int y = int.parse(x);
+
+  //   print("The integer value is: $y");
+  //
+  // dynamic x = 567;
+  // print(x);
+  // x = "rishi";
+  // print(x);
+  // dynamic x = 567;
+  // print(x);
+  // x = 6734.5896;
+  // print(x);
+  //Write a Dart program that stores student details using
+  //different data types and prints them in a formatted sentence.
+  var x = "rishi";
+  var y = 50;
+  var e = 7.67;
+  print("$x $y $e");
 }
